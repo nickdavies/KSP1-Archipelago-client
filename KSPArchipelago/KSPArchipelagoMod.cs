026bee386d46aaced5d6d111933b9be45eb0d599
Nick Davies <git@nicolasdavies.com.au>
2024-02-03 15:14:42 -0700

    Add basic shell of empty mod

---
+++ KSPArchipelago/KSPArchipelagoMod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//using System.IO;
+using System.Windows;
+using System.Runtime.InteropServices;
+
+using UnityEngine;
+using KSP.UI.Screens;
+
+
+
+namespace KSPArchipelago
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class KSPArchipelagoMod : MonoBehaviour
+    {
+
+        [DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
+        private void Start()
+        {
+            Debug.LogWarning("Start");
+            DontDestroyOnLoad(this);
+            AllocConsole();
+        }
+    }
+}
